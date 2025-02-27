15bbac927ce557aae5be16344650ae2f68b59a31
Sebastian Home <sebastianmoldrup@Sebastians-Air.home>
2025-02-27 22:26:28 +0100

    completed 1.js

--- 2625.js
+++ 2625.js
@@ -1,13 +1,15 @@
+// Failed attempt at solving the problem (Too much time on n = 1000)
 // Given a multi-dimensional array arr and a depth n, return a flattened version of that array.
+// Comment out arr & n to test different examples
 
 // Example 1:
-// const arr = [1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]];
-// const n = 0;
+const arr = [1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]];
+const n = 0;
 // output: [1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]]
 
 // Example 2:
-const arr = [1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]]
-const n = 1
+// const arr = [1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]]
+// const n = 1
 // output: [1, 2, 3, 4, 5, 6, 7, 8, [9, 10, 11], 12, 13, 14, 15]
 
 // Example 3:
@@ -16,37 +18,25 @@ const n = 1
 // output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
 const flat = (arr, n) => {
-  // Safeguard
+  // safeguard - return arr if n is 0
   if (n === 0) return arr;
-  
-  let result = [];
-  // console.log(result);
 
-  // loop through depth
-  // for (let i = 0; i < n; i++) {
-  //   // Loop through the result array
-  //   for (let j = 0; j < result.length; j++) {
-  //     // If the index is an array, loop through it and push the elements
-  //     if (Array.isArray(result[j])) {
-  //       for (let k = 0; k < j.length; k++) {
-  //         result.push(j[k]);
-  //     }
-  //     result.push(j);
-  //   }
-  // }
+  // Create a shallow copy
+  let result = arr.slice();
 
+  // Iterate through the array n times
   for (let i = 0; i < n; i++) {
-    for (let j = 0; j < arr.length; j++) {
-      if (Array.isArray(j)) {
-        console.log(j);
+    let temp = [];
+
+    for (let j = 0; j < result.length; j++) {
+      if (Array.isArray(result[j])) {
+        temp = temp.concat(result[j]);
+      } else {
+        temp.push(result[j]);
       }
-      // result.push(j);
-      console.log(j);
     }
+    result = temp;
   }
-
   return result;
-};
-
-
-console.log( flat(arr, n) );
+}
+console.log(flat(arr, n));
